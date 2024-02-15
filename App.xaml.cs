085bd0611fe6bb124ed5a62e298de0a22883a7d2
Jonathon Kischuk <107976831+jonathonkischuk@users.noreply.github.com>
2024-02-14 19:20:57 -0500

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GroceryApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
